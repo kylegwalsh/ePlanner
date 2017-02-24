2de3fddf33665ff4649c2057a010edcc5a4b7fb1
konradpabjan <konradpabjan@outlook.com>
2017-02-23 23:38:49 -0500

    Work on general layout and tabs

--- source.js
+++ source.js
@@ -1,3 +1,15 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+function loadToDo() {
+    document.getElementById("Main").innerHTML = "This is going to be the Todo";
+}
+function loadCompleted() {
+    document.getElementById("Main").innerHTML = "Completed Section!!!";
+}
+function loadCalendar() {
+    document.getElementById("Main").innerHTML = "Calendar Section!!!";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#To-do').addEventListener('click', loadToDo);
+  document.querySelector('#Completed').addEventListener('click', loadCompleted);
+  document.querySelector('#Calendar').addEventListener('click', loadCalendar);
+});
